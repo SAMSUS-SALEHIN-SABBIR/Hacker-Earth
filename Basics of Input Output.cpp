76ec5b572eb848137a2a012f409df49ef92afe0f
Samsus Salehin Sabbir <34869213+SAMSUS-SALEHIN-SABBIR@users.noreply.github.com>
2018-01-17 10:06:04 +0600

    Create Basics of Input Output.cpp

---
+++ Basics of Input Output.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int num;
+	string name ;
+	cin >> num;
+	cin >> name;
+	cout << num*2 << "\n";
+	cout << name << endl; 
+	return 0;
+}
